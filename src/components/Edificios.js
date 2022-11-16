94fedfd7f8689fc53301b337cecb10636b90ad95
Pedro Verde - Peverdemarketing <108496219+Peverdemarketing@users.noreply.github.com>
2022-11-16 08:27:03 -0300

    Categorias, menu y itemlistcontainer, Navbar

--- src/components/Edificios.js
+++ src/components/Edificios.js
@@ -7,7 +7,7 @@ const Edificios = () => {
         <p>Tenemos edificios pensados para comprar y rentar, mejor conocidos como multifamily.</p>
         <img src='https://peverdemarketing.com/wp-content/uploads/2022/10/Edificos-1.png'></img>
         <br></br>
-        <a href='/'>Más información</a>
+        <a href='/'>Más información </a>
     </div>
   )
 }
